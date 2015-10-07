f3cbc46801ef6bc4b0392f88745ae7ae5f2a87e9
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-10-06 20:34:25 -0500

    Finish building sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,25 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
+  def create # create login session
+    user = User.find_by_email(params[:email])
+    if user.present? && user.authenticate(params[:password])
+      set_user_session(user)
+      redirect_to links_path, notice: "Successfully logged in!"
+    else
+      flash[:alert] = 'Username or Password did not match.'
+      render :new
+    end
   end
 
   def destroy
+    session[:logged_in_user] = nil
+    redirect_to root_path
+  end
+
+  private
+
+  def set_user_session(user)
+    session[:logged_in_user] = user.id
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :links
   resources :users
 
+  root "links#index"
 end
