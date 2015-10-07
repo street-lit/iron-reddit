776fa146f29b0ce5aea68584f895ccfa8f078f66
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-10-06 20:05:06 -0500

    Change 'session controller' to 'sessions controller'

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'session/new'
-  get 'session/create'
-  get 'session/destroy'
+  get 'sessions/new',     to: 'sessions#new',     as: 'new_login'
+  get 'sessions/create',  to: 'sessions#create',  as: 'login'
+  get 'sessions/destroy', to: 'sessions#destroy', as: 'logout'
 
   resources :votes
   resources :links
   resources :users
+
 end
